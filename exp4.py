67fb645d1fb117dec7888799fe821cd7fda70a57
varshinidevaraju <144870750+varshinidevaraju@users.noreply.github.com>
2025-04-30 11:30:33 +0530

    Add files via upload

---
+++ exp4.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[25]:
+
+
+image = cv2.imread('chennai.jpg')
+
+
+# In[26]:
+
+
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)) 
+plt.title("Original Image")  
+plt.axis('off')
+
+
+# In[27]:
+
+
+tx, ty = 100, 50 
+M_translation = np.float32([[1, 0, tx], [0, 1, ty]])  # Translation matrix: 
+# [1, 0, tx] - Horizontal shift by tx
+# [0, 1, ty] - Vertical shift by ty
+translated_image = cv2.warpAffine(image, M_translation, (image.shape[1], image.shape[0]))
+
+
+# In[28]:
+
+
+plt.imshow(cv2.cvtColor(translated_image, cv2.COLOR_BGR2RGB)) 
+plt.title("Translated Image")  
+plt.axis('off')
+
+
+# In[29]:
+
+
+fx, fy = 5.0, 2.0  # Scaling factors (1.5x scaling for both width and height)
+scaled_image = cv2.resize(image, None, fx=fx, fy=fy, interpolation=cv2.INTER_LINEAR)
+
+
+# In[30]:
+
+
+plt.imshow(cv2.cvtColor(scaled_image, cv2.COLOR_BGR2RGB))  # Display the scaled image
+plt.title("Scaled Image")  # Set title
+plt.axis('off')
+
+
+# In[31]:
+
+
+# Step 4: Image Shearing
+shear_matrix = np.float32([[1, 0.5, 0], [0.5, 1, 0]])  # Shearing matrix
+# The matrix shears the image by a factor of 0.5 in both x and y directions
+# [1, 0.5, 0] - Shear along the x-axis (horizontal)
+# [0.5, 1, 0] - Shear along the y-axis (vertical)
+sheared_image = cv2.warpAffine(image, shear_matrix, (image.shape[1], image.shape[0]))
+
+
+# In[32]:
+
+
+plt.imshow(cv2.cvtColor(sheared_image, cv2.COLOR_BGR2RGB))  # Display the sheared image
+plt.title("Sheared Image")  # Set title
+plt.axis('off')
+
+
+# In[33]:
+
+
+reflected_image = cv2.flip(image, 2) 
+
+
+# In[34]:
+
+
+plt.imshow(cv2.cvtColor(reflected_image, cv2.COLOR_BGR2RGB))  # Display the reflected image
+plt.title("Reflected Image")  # Set title
+plt.axis('off')
+
+
+# In[35]:
+
+
+(height, width) = image.shape[:2]  # Get the image height and width
+angle = 45  # Rotation angle in degrees (rotate by 45 degrees)
+center = (width // 2, height // 2)  # Set the center of rotation to the image center
+M_rotation = cv2.getRotationMatrix2D(center, angle, 1)  # Get the rotation matrix
+# getRotationMatrix2D: Takes the center of rotation, angle, and scale factor (1 means no scaling)
+rotated_image = cv2.warpAffine(image, M_rotation, (width, height))  # Apply rotation
+
+
+# In[36]:
+
+
+plt.imshow(cv2.cvtColor(rotated_image, cv2.COLOR_BGR2RGB))  # Display the rotated image
+plt.title("Rotated Image")  # Set title
+plt.axis('off')
+
+
+# In[37]:
+
+
+x, y, w, h = 200,0, 150, 150  # Define the top-left corner (x, y) and the width (w) and height (h) of the crop
+# Cropping the image from coordinates (x, y) to (x+w, y+h)
+cropped_image = image[y:y+h, x:x+w]
+# The crop is performed by slicing the image array in the y and x directions
+
+
+# In[38]:
+
+
+plt.imshow(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))  # Display the cropped image
+plt.title("Cropped Image")  # Set title
+plt.axis('off')
+
+
+# In[ ]:
+
+
+
+
